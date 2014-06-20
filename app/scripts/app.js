440dd3e97107b2d8e05c9df8a3d2e518bd68c044
Josh Mu <mu@joshmu.com>
2014-06-20 18:17:04 +1000

    messing more with parallax with diff screens

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,41 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+	'ui.router'
+  ])
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+	//
+	// For any unmatched url, redirect to /state1
+	$urlRouterProvider.otherwise("/");
+	//
+	// Now set up the states
+	$stateProvider
+		.state('main', {
+			url: "/",
+			templateUrl: "views/main.html"
+		})
+//		.state('state1.list', {
+//			url: "/list",
+//			templateUrl: "partials/state1.list.html",
+//			controller: function($scope) {
+//				$scope.items = ["A", "List", "Of", "Items"];
+//			}
+//		})
+//		.state('state2', {
+//			url: "/state2",
+//			templateUrl: "partials/state2.html"
+//		})
+//		.state('state2.list', {
+//			url: "/list",
+//			templateUrl: "partials/state2.list.html",
+//			controller: function($scope) {
+//				$scope.things = ["A", "Set", "Of", "Things"];
+//			}
+//		})
+});
 
 /*
   .config(function ($routeProvider) {
--- app/scripts/directives/parallax.js
+++ app/scripts/directives/parallax.js
@@ -16,9 +16,40 @@ angular.module('paperlessApp')
 
 				var $window = $(window);
 
-				$window.scroll(function() {
+				var offset = +attrs.parallaxOffset || 0;
 
-					var yPos = (($window.scrollTop() - $elem.offset().top) / attrs.parallax) + (+attrs.parallaxoffset || 0);
+				var diff = $window.scrollTop() - $elem.offset().top;
+
+				var parallax = +attrs.parallax;
+
+//				var maxOffset = ($window.height() - ($elem.height() * attrs.parallax)) / attrs.parallax;
+//				var maxOffset = ($window.height()+($elem.height()*attrs.parallax)) / $elem.height();
+//
+//				console.log('window height is:', $window.height());
+//				console.log('element height is:', $elem.height());
+//
+//				console.log('offset chosen is:', offset);
+//				console.log('max offset is:', maxOffset);
+//
+//				offset = offset > maxOffset ? maxOffset : offset;
+//
+//				console.log('offset will be:', offset);
+//	    		console.log('max offset is:', maxOffset);
+
+				//TODO: listen for screen resize and recalc offset
+				//TODO: NEED TO REMOVE OFFSET OPTION AND CREATE FUNC TO CALC MIN PARALLAX AND MAX
+
+				//TODO: hack to prevent parallax on smaller screens unless very tall images... needs work!
+				//prevent small
+				if($window.width() < 767) {
+					parallax = 0;
+					offset = 0;
+				}
+
+				$window.scroll(function() {
+					var heightPos = $window.scrollTop() - $elem.offset().top;
+
+					var yPos = (heightPos / parallax) - offset;
 
 					// Put together our final background position
 					var coords = '50% ' + yPos + 'px';
