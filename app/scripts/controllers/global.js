d253ab766e77572f16ae1cdc9ab9c53b19cc43cc
Josh Mu <mu@joshmu.com>
2014-06-23 13:49:06 +1000

    added animate.css

---
+++ app/scripts/controllers/global.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paperlessApp.controller:GlobalCtrl
+ * @description
+ * # GlobalCtrl
+ * Controller of the paperlessApp
+ */
+angular.module('paperlessApp')
+  .controller('GlobalCtrl', function ($scope) {
+
+		$scope.bannerView = true;
+  });
