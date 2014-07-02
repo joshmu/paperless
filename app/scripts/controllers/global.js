75189126405e602a70bbd586ada45bb559cfe0b5
Josh Mu <mu@joshmu.com>
2014-07-02 18:43:52 +1000

    alliances

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -11,25 +11,24 @@ angular.module('paperlessApp')
 	.controller('GlobalCtrl', function($scope) {
 		var g = this;
 
-
 		/*#############################################################
-		BRANDS TABS
-		#############################################################*/
+		 BRANDS TABS
+		 #############################################################*/
 		g.brands = {
 			digitalposters: true,
-			eventranet : false,
-			speakerprep : false
+			eventranet    : false,
+			speakerprep   : false
 
 		};
 
-		g.updateBrand = function(brand){
+		g.updateBrand = function(brand) {
 			console.log('updating brand content');
 			if(g.brands[brand]) {
 				console.log('brand content is already in view');
 				return;
 			}
 			//set prev tab to false
-			angular.forEach(g.brands, function(val, key){
+			angular.forEach(g.brands, function(val, key) {
 				if(g.brands[key]) {
 					console.log('removing ' + key);
 					g.brands[key] = false;
@@ -40,18 +39,16 @@ angular.module('paperlessApp')
 			g.brands[brand] = true;
 		};
 
-
-
 		/*#############################################################
-		BANNER DETECTION
-		#############################################################*/
+		 BANNER DETECTION
+		 #############################################################*/
 
 		//auto-presume banner is going to be visible on load
 		g.bannerView = true;
 		g.initialState = true;
 
 		//watcher to check state and remove once state changes
-		var bannerListener = $scope.$watch(function(){
+		var bannerListener = $scope.$watch(function() {
 			return g.bannerView;
 		}, function(n) {
 			if(n === false) {
@@ -62,11 +59,10 @@ angular.module('paperlessApp')
 			}
 		});
 
-
 		//help bring in nav if page is reloaded and viewer is somewhere else on page
-		var midSectionListener = $scope.$watch(function(){
+		var midSectionListener = $scope.$watch(function() {
 			return g.midSectionView;
-		}, function(n){
+		}, function(n) {
 			if(n) {
 				console.log('mid section content detected');
 				g.initialState = false;
@@ -77,54 +73,85 @@ angular.module('paperlessApp')
 		});
 
 		function removeInitialStateWatchers() {
-				bannerListener();
-				midSectionListener();
+			bannerListener();
+			midSectionListener();
 		}
 
 		/*#############################################################
-		GOOGLE MAPS
-		#############################################################*/
+		 GOOGLE MAPS
+		 #############################################################*/
 
 		//brisbane
 		var brisMarker = {
 			coords: {
-				latitude: -27.4073899,
+				latitude : -27.4073899,
 				longitude: 153.0028595
 			},
-			name : 'bris'
+			name  : 'bris'
 		};
 
 		//melbourne
 		var melbMarker = {
 			//this coords are correct to the po box
 			coords: {
-				latitude: -37.813165,
+				latitude : -37.813165,
 				longitude: 144.955814
 			},
-			name : 'melb'
+			name  : 'melb'
 		};
 
 		//sydney
 		var sydMarker = {
 			coords: {
-				latitude: -33.7969235,
+				latitude : -33.7969235,
 				longitude: 150.9224326
 			},
-			name : 'syd'
+			name  : 'syd'
 		};
 
 		g.map = {
-			center: {
-				latitude: -33,
+			center    : {
+				latitude : -33,
 				longitude: 150
 			},
-			zoom: 5,
-			markerIcon : '../images/icons/letter_p.png',
-			markers : [melbMarker, brisMarker, sydMarker]
+			zoom      : 5,
+			markerIcon: '../images/icons/letter_p.png',
+			markers   : [melbMarker, brisMarker, sydMarker]
 
 		};
 
 		//default contact
 		g.currentContact = 'melb';
+
+		/*#############################################################
+		 SLIDER
+		 #############################################################*/
+		g.slider = {};
+		g.slider.images = [
+			{
+				src  : '../images/alliances/CSServices.jpg',
+				title: 'CSServices'
+			},
+			{
+				src  : '../images/alliances/Saffron.png',
+				title: 'Saffron'
+			},
+			{
+				src  : '../images/alliances/RombusDigital.jpg',
+				title: 'Rhombus Digital'
+			},
+			{
+				src  : '../images/alliances/IrisAV.png',
+				title: 'Iris AV'
+			},
+			{
+				src  : '../images/alliances/Naomi.png',
+				title: 'Naomi Jane Audio'
+			},
+			{
+				src  : '../images/alliances/Carlton_Terrace.png',
+				title: 'Carlton Terrace'
+			}
+		];
 
 	});
---
+++ app/scripts/directives/slider.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name paperlessApp.directive:slider
+ * @description
+ * # slider
+ */
+angular.module('paperlessApp')
+  .directive('slider', function () {
+    return {
+      restrict: 'AE',
+	  templateUrl: 'views/slider.html',
+	  scope: {
+		  images : '='
+	  },
+      link: function postLink(scope, element, attrs) {
+      }
+    };
+  });
