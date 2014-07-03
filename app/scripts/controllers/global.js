476f4a47b637e5bec365ed1bf01a047d8b22ebd9
Josh Mu <mu@joshmu.com>
2014-07-03 17:06:01 +1000

    prettify global.js

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -8,150 +8,145 @@
  * Controller of the paperlessApp
  */
 angular.module('paperlessApp')
-	.controller('GlobalCtrl', function($scope) {
-		var g = this;
+    .controller('GlobalCtrl', function($scope) {
+        var g = this;
 
-		/*#############################################################
+        /*#############################################################
 		 BRANDS TABS
 		 #############################################################*/
-		g.brands = {
-			digitalposters: true,
-			eventranet    : false,
-			speakerprep   : false
+        g.brands = {
+            digitalposters: true,
+            eventranet: false,
+            speakerprep: false
 
-		};
+        };
 
-		g.updateBrand = function(brand) {
-			console.log('updating brand content');
-			if(g.brands[brand]) {
-				console.log('brand content is already in view');
-				return;
-			}
-			//set prev tab to false
-			angular.forEach(g.brands, function(val, key) {
-				if(g.brands[key]) {
-					console.log('removing ' + key);
-					g.brands[key] = false;
-				}
-			});
-			//set new tab
-			console.log('setting ' + brand);
-			g.brands[brand] = true;
-		};
 
-		/*#############################################################
+
+        g.updateBrand = function(brand) {
+            console.log('updating brand content');
+            if (g.brands[brand]) {
+                console.log('brand content is already in view');
+                return;
+            }
+            //set prev tab to false
+            angular.forEach(g.brands, function(val, key) {
+                if (g.brands[key]) {
+                    console.log('removing ' + key);
+                    g.brands[key] = false;
+                }
+            });
+            //set new tab
+            console.log('setting ' + brand);
+            g.brands[brand] = true;
+        };
+
+        /*#############################################################
 		 BANNER DETECTION
 		 #############################################################*/
 
-		//auto-presume banner is going to be visible on load
-		g.bannerView = true;
-		g.initialState = true;
+        //auto-presume banner is going to be visible on load
+        g.bannerView = true;
+        g.initialState = true;
 
-		//watcher to check state and remove once state changes
-		var bannerListener = $scope.$watch(function() {
-			return g.bannerView;
-		}, function(n) {
-			if(n === false) {
-				console.log('banner not visible!');
-				g.initialState = false;
-				//remove watcher
-				removeInitialStateWatchers();
-			}
-		});
+        //watcher to check state and remove once state changes
+        var bannerListener = $scope.$watch(function() {
+            return g.bannerView;
+        }, function(n) {
+            if (n === false) {
+                console.log('banner not visible!');
+                g.initialState = false;
+                //remove watcher
+                removeInitialStateWatchers();
+            }
+        });
 
-		//help bring in nav if page is reloaded and viewer is somewhere else on page
-		var midSectionListener = $scope.$watch(function() {
-			return g.midSectionView;
-		}, function(n) {
-			if(n) {
-				console.log('mid section content detected');
-				g.initialState = false;
-				g.bannerView = false;
-				//remove initial state watchers
-				removeInitialStateWatchers();
-			}
-		});
+        //help bring in nav if page is reloaded and viewer is somewhere else on page
+        var midSectionListener = $scope.$watch(function() {
+            return g.midSectionView;
+        }, function(n) {
+            if (n) {
+                console.log('mid section content detected');
+                g.initialState = false;
+                g.bannerView = false;
+                //remove initial state watchers
+                removeInitialStateWatchers();
+            }
+        });
 
-		function removeInitialStateWatchers() {
-			bannerListener();
-			midSectionListener();
-		}
+        function removeInitialStateWatchers() {
+            bannerListener();
+            midSectionListener();
+        }
 
-		/*#############################################################
+        /*#############################################################
 		 GOOGLE MAPS
 		 #############################################################*/
 
-		//brisbane
-		var brisMarker = {
-			coords: {
-				latitude : -27.4073899,
-				longitude: 153.0028595
-			},
-			name  : 'bris'
-		};
+        //brisbane
+        var brisMarker = {
+            coords: {
+                latitude: -27.4073899,
+                longitude: 153.0028595
+            },
+            name: 'bris'
+        };
 
-		//melbourne
-		var melbMarker = {
-			//this coords are correct to the po box
-			coords: {
-				latitude : -37.813165,
-				longitude: 144.955814
-			},
-			name  : 'melb'
-		};
+        //melbourne
+        var melbMarker = {
+            //this coords are correct to the po box
+            coords: {
+                latitude: -37.813165,
+                longitude: 144.955814
+            },
+            name: 'melb'
+        };
 
-		//sydney
-		var sydMarker = {
-			coords: {
-				latitude : -33.7969235,
-				longitude: 150.9224326
-			},
-			name  : 'syd'
-		};
+        //sydney
+        var sydMarker = {
+            coords: {
+                latitude: -33.7969235,
+                longitude: 150.9224326
+            },
+            name: 'syd'
+        };
 
-		g.map = {
-			center    : {
-				latitude : -33,
-				longitude: 150
-			},
-			zoom      : 5,
-			markerIcon: '../images/icons/letter_p.png',
-			markers   : [melbMarker, brisMarker, sydMarker]
+        g.map = {
+            center: {
+                latitude: -33,
+                longitude: 150
+            },
+            zoom: 5,
+            markerIcon: '../images/icons/letter_p.png',
+            markers: [melbMarker, brisMarker, sydMarker]
 
-		};
+        };
 
-		//default contact
-		g.currentContact = 'melb';
+        //default contact
+        g.currentContact = 'melb';
 
-		/*#############################################################
+        /*#############################################################
 		 SLIDER
 		 #############################################################*/
-		g.slider = {};
-		g.slider.images = [
-			{
-				src  : '../images/alliances/CSServices.jpg',
-				title: 'CSServices'
-			},
-			{
-				src  : '../images/alliances/Saffron.png',
-				title: 'Saffron'
-			},
-			{
-				src  : '../images/alliances/RombusDigital.jpg',
-				title: 'Rhombus Digital'
-			},
-			{
-				src  : '../images/alliances/IrisAV.png',
-				title: 'Iris AV'
-			},
-			{
-				src  : '../images/alliances/Naomi.png',
-				title: 'Naomi Jane Audio'
-			},
-			{
-				src  : '../images/alliances/Carlton_Terrace.png',
-				title: 'Carlton Terrace'
-			}
-		];
+        g.slider = {};
+        g.slider.images = [{
+            src: '../images/alliances/CSServices.jpg',
+            title: 'CSServices'
+        }, {
+            src: '../images/alliances/Saffron.png',
+            title: 'Saffron'
+        }, {
+            src: '../images/alliances/RombusDigital.jpg',
+            title: 'Rhombus Digital'
+        }, {
+            src: '../images/alliances/IrisAV.png',
+            title: 'Iris AV'
+        }, {
+            src: '../images/alliances/Naomi.png',
+            title: 'Naomi Jane Audio'
+        }, {
+            src: '../images/alliances/Carlton_Terrace.png',
+            title: 'Carlton Terrace'
+        }];
 
-	});
+    });
